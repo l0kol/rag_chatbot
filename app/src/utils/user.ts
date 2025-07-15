fd4c843d88b222a7c558a934ad6e0ba77dafd450
Luka Levac <luka.levac@gmail.com>
2025-07-15 23:41:37 +0200

    user id from uuid

--- app/src/utils/user.ts
+++ app/src/utils/user.ts
@@ -1,8 +1,12 @@
+import { v4 as uuidv4 } from "uuid";
+
 export function getOrCreateUserId(): string {
   let userId = localStorage.getItem("user_id");
+
   if (!userId) {
-    userId = crypto.randomUUID();
+    userId = uuidv4();
     localStorage.setItem("user_id", userId);
   }
+
   return userId;
 }
