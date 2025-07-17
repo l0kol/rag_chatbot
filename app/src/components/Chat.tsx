7957f95bd5a7be8b6eec027ec58e6dde6787590e
Luka Levac <luka.levac@gmail.com>
2025-07-17 20:41:06 +0200

    build fix

--- app/src/components/Chat.tsx
+++ app/src/components/Chat.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, use } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useAppContext } from "../context/AppContext";
 
 type Message = {
